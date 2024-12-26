351e9a9424f2f9286c88b6e501c0cab787c4e882
ERICK-KRAMER <kramerfilho@hotmail.com>
2024-12-26 17:58:12 -0300

    add walltRepository

---
+++ Data/Repository/Wallet/IWalletRepository.cs
@@ -0,0 +1,12 @@
+﻿using PicPay.Model.Entity;
+
+namespace PicPay.Data.Repository.Wallet;
+
+public interface IWalletRepository
+{
+    Task AddAsync(WalletEntity wallet);
+    Task UpdateAsync(WalletEntity wallet);
+    Task<WalletEntity?> GetByCpfAsync(string cpf, string email);
+    Task<WalletEntity?> GetByIdAsync(Guid id);
+    Task CommitAsync();
+}
---
+++ Data/Repository/Wallet/WalletRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PicPay.Model.Entity;
+
+namespace PicPay.Data.Repository.Wallet;
+
+public class WalletRepository : IWalletRepository
+{
+    private readonly PicPayDbContext _context;
+
+    public WalletRepository(PicPayDbContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task AddAsync(WalletEntity wallet)
+    {
+        await _context.Wallet.AddAsync(wallet);
+    }
+
+    public async Task UpdateAsync(WalletEntity wallet)
+    {
+        _context.Wallet.Update(wallet);
+    }
+
+    public Task<WalletEntity?> GetByCpfAsync(string cpf, string email)
+    {
+        var wallet = _context.Wallet.FirstOrDefaultAsync(
+            x => x.Cpf.Equals(cpf) && x.Email.Equals(email)
+        );
+        return wallet;
+    }
+
+    public async Task<WalletEntity?> GetByIdAsync(Guid id)
+    {
+        var wallet = await _context.Wallet.FindAsync(id);
+        return wallet;
+    }
+
+    public async Task CommitAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
