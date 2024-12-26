95289f223e54288b438abb4bcff0104bd01f00bb
ERICK-KRAMER <kramerfilho@hotmail.com>
2024-12-26 18:30:45 -0300

    add transferController

---
+++ Controller/TransferController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PicPay.Model.Request;
+using PicPay.Services.Authorization;
+using PicPay.Services.Transfer;
+
+namespace PicPay.Controller;
+
+[ApiController]
+[Route("[controller]")]
+public class TransferController : ControllerBase
+{
+    private readonly ITransferService _service;
+    private readonly IAuthorizationService _authorizationService;
+
+    public TransferController(ITransferService service, IAuthorizationService authorizationService)
+    {
+        _service = service;
+        _authorizationService = authorizationService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Transfer(TransferRequest request)
+    {
+        var transfer = await _service.TransferAsync(request);
+
+        if (!transfer.IsSuccess)
+        {
+            return BadRequest();
+        }
+        
+        return Ok(transfer);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> TestAuthorization()
+    {
+        var response = await _authorizationService.AuthorizatorAsync();
+        return Ok(response);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,11 @@
 using Microsoft.EntityFrameworkCore;
 using PicPay.Data;
+using PicPay.Data.Repository.Transfer;
+using PicPay.Data.Repository.Wallet;
+using PicPay.Services.Authorization;
+using PicPay.Services.Notification;
+using PicPay.Services.Transfer;
+using PicPay.Services.Wallet;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +15,14 @@ builder.Services.AddSwaggerGen();
 //Database config
 var connectionString =  builder.Configuration.GetConnectionString("PicPayDb");
 builder.Services.AddDbContext<PicPayDbContext>(options => options.UseNpgsql(connectionString));
+
+//dependency injection
+builder.Services.AddScoped<IWalletRepository, WalletRepository>();
+builder.Services.AddScoped<ITransferRepository, TransferRepository>();
+builder.Services.AddScoped<IWalletService, WalletService>();
+builder.Services.AddScoped<ITransferService, TransferService>();
+builder.Services.AddHttpClient<IAuthorizationService, AuthorizationService>();
+builder.Services.AddScoped<INotificationService, NotificationService>();
 
 var app = builder.Build();
 
